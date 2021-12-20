f17030e2c456a1e86dfe25b57a0183ba0804fbf6
Badar Shafiq <badarjanjua92@gmail.com>
2021-12-20 19:57:51 +0500

    Added api version and required endpoints

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  resources :tasks, except: [:destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :tasks, except: [:destroy] do
+        member do
+          get :task_summary
+        end
+      end
+
+      post '/set_start_time/:task_id', to: 'user_tasks#set_start_time'
+      put '/set_end_time/:task_id', to: 'user_tasks#set_end_time'
+      get '/users_summary', to: 'user_tasks#users_summary'
+    end
+  end
 end
